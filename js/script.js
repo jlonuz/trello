a02ac7e90f6a09b1faee3228ba2f033a0541ce2e
Josefina <lonuz.j@gmail.com>
2016-11-07 13:56:55 -0300

    listo el título y el texarrea

--- js/script.js
+++ js/script.js
@@ -5,10 +5,9 @@ function anadirLista(){
 	var textoBoton = document.createTextNode("Guardar");
 
 	//asigno clases
-	inputFormulario.classList.add("input");
-	botonGuardar.classList.add("boton-guardar");
-
-
+	inputFormulario.setAttribute("id","rescatarValor");
+	inputFormulario.classList.add("input","form-control");
+	botonGuardar.classList.add("boton-guardar", "btn", "btn-default");
 
 
 	//asingo nodos hijos
@@ -16,7 +15,58 @@ function anadirLista(){
 	saveBox.appendChild(botonGuardar);
 	botonGuardar.appendChild(textoBoton);
 
+	//cuando presiono guarar sucede esto:
 
+	botonGuardar.addEventListener('click', function(){
+
+		var tituloLista = document.getElementById('rescatarValor').value;
+		document.getElementById('rescatarValor').value = "";
+
+		if(tituloLista == "" || tituloLista == null){
+		alert("Por favor, ingresa un título a tu lista");
+		return; 
+		}
+
+		//Aquí creo el esquema de como se imprimirá el valor del input
+		var contTarea = document.getElementById('contenedor');
+		var divCol = document.createElement('div');
+		var texto = document.createElement('h3');
+		var textoTarea = document.createTextNode(tituloLista);
+		var anadirTarjeta = document.createElement('a')
+		var textTarjeta = document.createTextNode("Añadir Tarjeta...");
+		
+
+		//Aquí les agregues sus respectivas clases
+
+	
+		divCol.classList.add("col-xs-4","col-ms-4","col-md-4");
+		texto.setAttribute('class','titulo-lista');
+
+		//Asigno cada nodo con su hijo
+		
+		contTarea.appendChild(divCol);
+		//divRow.appendChild(divCol);
+		divCol.appendChild(texto);
+		texto.appendChild(textoTarea);
+		divCol.appendChild(anadirTarjeta);
+		anadirTarjeta.appendChild(textTarjeta);
+
+
+
+   		anadirTarjeta.addEventListener('click', function(){
+
+   		var textArea = document.createElement('textarea');
+   		console.log('llegue');
+
+   		anadirTarjeta.appendChild(textArea);
+
+   		})
+
+
+
+	})
+
+	
 
 
 
